94e4454f8acaa0d1041b70d229d8c486c4068c54
Alfitrm <grackrev@gmail.com>
2025-09-05 19:23:49 +0700

    fix: Fix image path errors and improve offline ux

--- src/components/BookView.jsx
+++ src/components/BookView.jsx
@@ -7,9 +7,9 @@ const BookView = ({ book, currentPage, handleNextPage, handlePrevPage, imageErro
     const activePage = book.pages.find(p => p.page === currentPage);
 
     return (
-        <main className="flex flex-col items-center gap-4 md:w-6/12">
+        <main className="flex flex-col items-center gap-4 w-screen md:w-4/12">
             {imageError ? (
-                <div className="bg-red-50 p-4 w-1/3 h-[60vh] place-content-center rounded-lg text-center shadow">
+                <div className="bg-red-50 p-4 w-screen h-[60vh] place-content-center rounded-lg text-center shadow">
                     <h2 className="font-semibold text-red-700">Halaman belum diunduh.</h2>
                     <p className="text-sm">Coba online untuk melihat konten ini.</p>
                 </div>
@@ -18,28 +18,30 @@ const BookView = ({ book, currentPage, handleNextPage, handlePrevPage, imageErro
                     <Image
                         src={activePage?.image}
                         alt={`Halaman ${currentPage} dari ${book.title}`}
+                        width={1000}
+                        height={1000}
                         className="shadow w-full sm:object-contain object-cover h-[70vh] rounded-xl"
                         onError={() => setImageError(true)}
                     />
 
-                    <button
-                        onClick={handlePrevPage}
-                        disabled={currentPage === 1}
-                        className="absolute left-1/20 top-1/2 -translate-y-1/2 p-3 rounded-full bg-purple-1 text-white hover:bg-purple-2 disabled:opacity-40 disabled:cursor-not-allowed cursor-pointer transition-all"
-                    >
-                        <ArrowLeftIcon className="w-5 h-5" />
-                    </button>
-
-                    <button
-                        onClick={handleNextPage}
-                        disabled={currentPage === book.pages.length}
-                        className="absolute right-1/20 top-1/2 -translate-y-1/2 p-3 rounded-full bg-purple-1 text-white hover:bg-purple-2 disabled:opacity-40 disabled:cursor-not-allowed cursor-pointer transition-all"
-                    >
-                        <ArrowRightIcon className="w-5 h-5" />
-                    </button>
                 </div>
             )}
-            <div className="w-full h-2 bg-gray-200 rounded-full overflow-hidden">
+            <button
+                onClick={handlePrevPage}
+                disabled={currentPage === 1}
+                className="absolute left-2 top-1/2 -translate-y-1/2 p-3 rounded-full bg-purple-1 text-white hover:bg-purple-2 disabled:opacity-40 disabled:cursor-not-allowed cursor-pointer transition-all"
+            >
+                <ArrowLeftIcon className="w-5 h-5" />
+            </button>
+
+            <button
+                onClick={handleNextPage}
+                disabled={currentPage === book.pages.length}
+                className="absolute right-2 top-1/2 -translate-y-1/2 p-3 rounded-full bg-purple-1 text-white hover:bg-purple-2 disabled:opacity-40 disabled:cursor-not-allowed cursor-pointer transition-all"
+            >
+                <ArrowRightIcon className="w-5 h-5" />
+            </button>
+            <div className="w-9/12 h-2 bg-gray-200 rounded-full overflow-hidden">
                 <div
                     className="h-full bg-purple-1 transition-all duration-300"
                     style={{ width: `${(currentPage / book.pages.length) * 100}%` }}
