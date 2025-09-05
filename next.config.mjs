182ef7c8d6b08aa8a494538d3ee5ecc8cbc80b3a
Alfitrm <grackrev@gmail.com>
2025-09-05 20:33:28 +0700

    fix: install and config pwa library to ensure homepage is avaible in offline mode.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,27 @@
+import withPWAInit from "next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  runtimeCaching: [
+    {
+      urlPattern: /\/books\.json$/,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'data-cache',
+        expiration: {
+          maxEntries: 10,
+          maxAgeSeconds: 60 * 60 * 24 * 30,
+        },
+      },
+    },
+  ],
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
