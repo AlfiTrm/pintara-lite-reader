d07bac08d5915508707b3372648a797fb26ad008
Alfitrm <grackrev@gmail.com>
2025-09-05 23:44:44 +0700

    feat: style offline fallback page and finalize the UI.

--- src/pages/_offline.jsx
+++ src/pages/_offline.jsx
@@ -1,14 +1,10 @@
-import Head from 'next/head';
 
 export default function OfflinePage() {
     return (
         <>
-            <Head>
-                <title>Yahh.. Kamu Offline</title>
-            </Head>
             <div className="min-h-screen flex flex-col items-center justify-center text-center p-4">
-                <h1 className="text-4xl font-bold mb-4">Kamu Offline</h1>
-                <p className="text-lg">Koneksi internet-mu terputus. Silakan periksa kembali koneksimu.</p>
+                <h1 className="text-4xl font-bold mb-4 text-red-1">Yahh.. Kamu Offline</h1>
+                <p className="text-lg ">Koneksi internet-mu terputus. Silakan periksa kembali koneksimu.</p>
             </div>
         </>
     );
