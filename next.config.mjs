2c708d5de7c03a6e88357cf3bb79e78f9a366987
Alfitrm <grackrev@gmail.com>
2025-09-05 22:38:07 +0700

    chore: Simplify PWA config for final test.

--- next.config.mjs
+++ next.config.mjs
@@ -2,27 +2,7 @@ import withPWAInit from "next-pwa";
 
 const withPWA = withPWAInit({
   dest: "public",
-  register: true,
-  skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
-  fallbacks: {
-    document: '/_offline',
-  },
-  runtimeCaching: [
-    {
-      urlPattern: /\/books\.json$/,
-      handler: 'CacheFirst',
-      options: {
-        cacheName: 'data-cache-v1',
-        expiration: {
-          maxEntries: 1,
-          maxAgeSeconds: 60 * 60 * 24,
-        },
-      },
-    },
-  ],
-
-  buildExcludes: [/_next\/dynamic-css-manifest\.json/],
 });
 
 /** @type {import('next').NextConfig} */
