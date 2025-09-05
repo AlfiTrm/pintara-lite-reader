f6a31fe5a2f4250c05740e9abbbc7d346c29cb9c
Alfitrm <grackrev@gmail.com>
2025-09-05 20:41:38 +0700

    fix:config pwa library to ensure homepage is avaible in offline mode.

--- next.config.mjs
+++ next.config.mjs
@@ -4,19 +4,6 @@ const withPWA = withPWAInit({
   dest: "public",
   register: true,
   skipWaiting: true,
-  runtimeCaching: [
-    {
-      urlPattern: /\/books\.json$/,
-      handler: 'CacheFirst',
-      options: {
-        cacheName: 'data-cache',
-        expiration: {
-          maxEntries: 10,
-          maxAgeSeconds: 60 * 60 * 24 * 30,
-        },
-      },
-    },
-  ],
 });
 
 /** @type {import('next').NextConfig} */
